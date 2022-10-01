6669b75364a50077b3f1cdff20aca44d7aba6a03
Akshit Batra <akbatra567@gmail.com>
2022-10-02 04:23:27 +0530

    Added theme + basic code for home screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import './App.css';
-import CreateCrossword from './components/CreateCrossword/CreateCrossword';
+import './App.css'
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import HomeScreen from './components/homeScreen';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navbar from './components/common/navbar';
 
-function App() {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function App () {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = React.useState(prefersDarkMode?'dark':'light');
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <CreateCrossword/>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar ColorModeContext={ColorModeContext}/>
+        <HomeScreen/>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
-export default App;
+export default App
