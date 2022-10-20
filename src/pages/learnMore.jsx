06d3b6f3f0c7d6bfb01da27a6b8758b96c8f2732
Shashwata Saha <Shashwata.saha@Shashwatas-MacBook-Pro.local>
2022-10-20 09:49:24 +0530

    add: separate page for learn more and linking that

---
+++ src/pages/learnMore.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LearnMore() {
+  return (
+    <div className="container">
+      <Typography gutterBottom variant="h5" component="div">
+        Learn More
+      </Typography>
+    </div>
+  );
+}
+
+export default LearnMore;
